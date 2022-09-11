612919cea66236b9baab2f3df3bc71fa7cb0282f
Talita Marcondes <70537487+talitatodesco@users.noreply.github.com>
2022-09-11 17:23:28 -0300

    mudando

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const express = require('express');
 const app = express();
 
 //ROTA DE CADASTRO DE CATEGORIA
-app.post('/CadastrarCategoria', (req, res)=>{
-    res.send('ROTA DE CADASTRO DE CATEGORIA!!');
+app.post('/Cadastrardados', (req, res)=>{
+    res.send('ROTA DE CADASTRO DE DADOS!!');
 });
 
 //ROTA DE LEITURA DE CATEGORIA
-app.get('/ListarCategoria', (req, res)=>{
-    res.send('ROTA DE LISTAGEM DE CATEGORIA!!');
+app.get('/Listardados', (req, res)=>{
+    res.send('ROTA DE LISTAGEM DE DADOS!!');
 });
 
 //ROTA DE ALTERAÇÃO DE CATEGORIA
-app.put('/AlterarCategoria', (req, res)=>{
-    res.send('ROTA DE ALTERAÇÃO DE CATEGORIA!!');
+app.put('/Alterardados', (req, res)=>{
+    res.send('ROTA DE ALTERAÇÃO DE DADOS!!');
 });
 
 //ROTA DE EXLUSÃO DE CATEGORIA
-app.delete('/DeletarCategoria', (req, res)=>{
-    res.send('ROTA DE DELETAR CATEGORIA!!');
+app.delete('/Deletardados', (req, res)=>{
+    res.send('ROTA DE DELETAR DADOS!!');
 });
 
 
