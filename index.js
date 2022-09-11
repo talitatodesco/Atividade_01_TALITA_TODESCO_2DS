b1a2b4685074ba7e3ed0beca8d158e7a045eb0e7
Talita Marcondes <70537487+talitatodesco@users.noreply.github.com>
2022-09-11 18:15:06 -0300

    Retirando Comentarios

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require('express');
 
 const app = express();
 
-//ROTA DE CADASTRO DE CATEGORIA
+
 app.post('/Cadastrardados', (req, res)=>{
     res.send('ROTA DE CADASTRO DE DADOS!!');
 });
 
-//ROTA DE LEITURA DE CATEGORIA
+
 app.get('/Listardados', (req, res)=>{
     res.send('ROTA DE LISTAGEM DE DADOS!!');
 });
 
-//ROTA DE ALTERAÇÃO DE CATEGORIA
+
 app.put('/Alterardados', (req, res)=>{
     res.send('ROTA DE ALTERAÇÃO DE DADOS!!');
 });
 
-//ROTA DE EXLUSÃO DE CATEGORIA
+
 app.delete('/Deletardados', (req, res)=>{
     res.send('ROTA DE DELETAR DADOS!!');
 });
