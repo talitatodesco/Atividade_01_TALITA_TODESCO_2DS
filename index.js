03da73e1b875a2c487fdc53b1dbbd06717772f20
Talita Marcondes <70537487+talitatodesco@users.noreply.github.com>
2022-09-11 15:05:50 -0300

    Servidor   web   recebendo as requisições “HTTP”

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const express = require('express');
 
 const app = express();
 
+//ROTA DE CADASTRO DE CATEGORIA
+app.post('/CadastrarCategoria', (req, res)=>{
+    res.send('ROTA DE CADASTRO DE CATEGORIA!!');
+});
+
+//ROTA DE LEITURA DE CATEGORIA
+app.get('/ListarCategoria', (req, res)=>{
+    res.send('ROTA DE LISTAGEM DE CATEGORIA!!');
+});
+
+//ROTA DE ALTERAÇÃO DE CATEGORIA
+app.put('/AlterarCategoria', (req, res)=>{
+    res.send('ROTA DE ALTERAÇÃO DE CATEGORIA!!');
+});
+
+//ROTA DE EXLUSÃO DE CATEGORIA
+app.delete('/DeletarCategoria', (req, res)=>{
+    res.send('ROTA DE DELETAR CATEGORIA!!');
+});
+
+
 app.listen(3000, ()=>{
     console.log('Servidor Rodando!!')
 })
