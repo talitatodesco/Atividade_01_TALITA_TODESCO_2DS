16d8d65738b2df470d7c83aeadd6e55440dd2bf6
Talita Marcondes <70537487+talitatodesco@users.noreply.github.com>
2022-09-11 16:19:45 -0300

    Alteração.

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ app.put('/AlterarCategoria', (req, res)=>{
     res.send('ROTA DE ALTERAÇÃO DE CATEGORIA!!');
 });
 
-//ROTA DE EXLUSÃO DE CATEGORIA
-app.delete('/DeletarCategoria', (req, res)=>{
-    res.send('ROTA DE DELETAR CATEGORIA!!');
-});
 
 
 app.listen(3000, ()=>{
