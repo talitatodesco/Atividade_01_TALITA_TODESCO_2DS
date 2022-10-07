7c2073e7587cee80f8743c887ddf79e3f4fcdbe9
Talita Marcondes <70537487+talitatodesco@users.noreply.github.com>
2022-10-06 23:16:53 -0300

    teste

--- route/routeMusica.js
+++ route/routeMusica.js
@@ -1,27 +1,25 @@
 const express = require('express');
 
-const modelCategoria = require('../model/modelMusica');
+const modelmusica = require('../model/modelmusica');
 
-const routerMusica = express.Router();
-
+const router = express.Router();
 
 
 router.post('/Cadastrardados', (req, res)=>{
     res.send('ROTA DE CADASTRO DE DADOS!!');
 });
 
-
 router.get('/Listardados', (req, res)=>{
     res.send('ROTA DE LISTAGEM DE DADOS!!');
 });
-
 
 router.put('/Alterardados', (req, res)=>{
     res.send('ROTA DE ALTERAÇÃO DE DADOS!!');
 });
 
-
 router.delete('/Deletardados', (req, res)=>{
-    res.send('ROTA DE DELETAR DADOS!!');
+    res.send('ROTA DE DELETAR DADOS!! ');
 });
 
+
+module.exports = router;
