da67f28631f45040945de9a96b9eafa24ae34f2f
Talita Marcondes <70537487+talitatodesco@users.noreply.github.com>
2022-10-06 23:38:22 -0300

    Revert "Fazendo a conexão"
    This reverts commit 98b212dfffdd3300710bcc6843d358d627a5437b.

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const routerMusica = require('./route/routeMusica');
 
 const app = express();
 
 console.log('teste de github')
 
-app.use ('/', routerMusica);
+
+app.post('/Cadastrardados', (req, res)=>{
+    res.send('ROTA DE CADASTRO DE DADOS!!');
+});
 
 
+app.get('/Listardados', (req, res)=>{
+    res.send('ROTA DE LISTAGEM DE DADOS!!');
+});
+
+
+app.put('/Alterardados', (req, res)=>{
+    res.send('ROTA DE ALTERAÇÃO DE DADOS!!');
+});
+
+
+app.delete('/Deletardados', (req, res)=>{
+    res.send('ROTA DE DELETAR DADOS!!');
+});
 
 
 app.listen(3000, ()=>{
